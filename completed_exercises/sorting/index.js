fadb0a100d14c35e11b0483171bafef3edef493b
Ben Basuni <benbasuni1@gmail.com>
2020-11-10 09:42:57 -0500

    finished algocasts

--- completed_exercises/sorting/index.js
+++ completed_exercises/sorting/index.js
@@ -38,29 +38,26 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-  if (arr.length === 1) {
-    return arr;
-  }
+	if (arr.length === 1) return arr;
 
-  const center = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const right = arr.slice(center);
+	let mid = Math.floor(arr.length / 2);
+	let left = arr.slice(0, mid);
+	let right = arr.slice(mid);
 
-  return merge(mergeSort(left), mergeSort(right));
+	return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-  const results = [];
+	let results = [];
 
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      results.push(left.shift());
-    } else {
-      results.push(right.shift());
-    }
-  }
+	while (left.length && right.length) {
+		if (left[0] < right[0]) 
+			results.push(left.shift());
+		else 
+			results.push(right.shift());
+	}
 
-  return [...results, ...left, ...right];
+	return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
